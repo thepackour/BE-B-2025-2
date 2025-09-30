14112b3ffb0ba68fa8ed485cfe6efb4fdc228c64
thepackour <thepackour5610@gmail.com>
2025-09-30 11:31:01 +0900

    feat: Controller 필드 추가
    유틸리티 클래스이므로 인스턴스 생성은 막아둠

---
+++ src/main/java/racingcar/Controller.java
@@ -0,0 +1,10 @@
+package racingcar;
+
+import java.util.ArrayList;
+
+public class Controller {
+    private static ArrayList<String> car_name_list = new ArrayList<>();
+    private static int loop;
+
+    private Controller() {}
+}
