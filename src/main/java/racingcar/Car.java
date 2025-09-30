f4987a553cfc354f1283151e6e606996ca88d92f
thepackour <thepackour5610@gmail.com>
2025-09-30 20:24:58 +0900

    feat(Car): get_cars() 추가

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -7,6 +7,8 @@ public class Car {
     private int moved = 0;
     private static ArrayList<Car> cars = new ArrayList<Car>();
 
+    public static ArrayList<Car> get_cars() { return cars; }
+
     public String get_name() { return name; }
 
     public int get_moved() { return moved; }
