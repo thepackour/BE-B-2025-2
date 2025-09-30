3e5f69c01639dc0550126bf72332a1cac8cf3c3b
thepackour <thepackour5610@gmail.com>
2025-09-30 16:22:01 +0900

    feat(Controller): add_car_names() 추가
    입력 받은 문자열을 콤마로 구분하고 5자가 넘는 이름이 있는 경우 에외 throw

--- src/main/java/racingcar/Controller.java
+++ src/main/java/racingcar/Controller.java
@@ -1,10 +1,23 @@
 package racingcar;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Controller {
     private static ArrayList<String> car_name_list = new ArrayList<>();
     private static int loop;
+    private static Scanner input = new Scanner(System.in);
+
+    public static void add_car_names(String name) {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
+        String[] names = input.nextLine().split(",");
+        Arrays.stream(names)
+                .forEach(s -> {
+                    if (s.length() > 5) throw new IllegalArgumentException();
+                    else car_name_list.add(s);
+                });
+    }
 
     private Controller() {}
 }
