fdbf66b9ebf946aa256d7689eebf0371a5a100a2
thepackour <thepackour5610@gmail.com>
2025-09-30 21:11:05 +0900

    feat(Controller): print_winners() 추가
    지금은 print_뭐시기 메서드가 데이터 구하는거랑 출력까지 다 하고 있는데 분리해야 할 수도?

--- src/main/java/racingcar/Controller.java
+++ src/main/java/racingcar/Controller.java
@@ -1,9 +1,14 @@
 package racingcar;
 
+import javax.swing.text.html.Option;
+
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class Controller {
     private static ArrayList<String> car_name_list = new ArrayList<>();
@@ -12,6 +17,7 @@ public class Controller {
     public static void add_car_names() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
         String[] names = readLine().split(",");
+        if (names.length == 0) throw new IllegalArgumentException();
         Arrays.stream(names)
                 .forEach(s -> {
                     if (s.length() > 5) throw new IllegalArgumentException();
@@ -35,6 +41,21 @@ public class Controller {
             System.out.println("-".repeat(moved));
         }
         System.out.println();
+    }
+
+    public static void print_winners() {
+        ArrayList<Car> cars = Car.get_cars();
+        Optional<Integer> max = cars.stream()
+                .map(Car::get_moved)
+                .max(Integer::compareTo);
+        if (max.isEmpty()) System.out.println("오류가 발생했습니다.");
+        else {
+            String res = cars.stream()
+                    .filter(car -> car.get_moved() == max.get())
+                    .map(Car::get_name)
+                    .collect(Collectors.joining(", "));
+            System.out.println("최종 우승자 : " + res);
+        }
     }
 
     private Controller() {}
