05e6d22e956d2a287c5bdb2a591f5bc39f699579
thepackour <thepackour5610@gmail.com>
2025-09-30 21:20:57 +0900

    feat(Car): move_cars(), move() 추가
    깜빡하고 차 움직이는 메서드 안 넣어서 추가함, 외부에서는 호출할 수 없고 move_cars()를 통해서만 사용, 랜덤 값은 move_cars()에서 정하고 move()는 움직이는 기능만 수행

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,17 +1,27 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.ArrayList;
 
 public class Car {
     private final String name;
     private int moved = 0;
-    private static ArrayList<Car> cars = new ArrayList<Car>();
+    private static ArrayList<Car> cars = new ArrayList<>();
 
     public static ArrayList<Car> get_cars() { return cars; }
+
+    public static void move_cars() {
+        for (Car car : cars) {
+            if (Randoms.pickNumberInRange(0, 9) >= 4) car.move();
+        }
+    }
 
     public String get_name() { return name; }
 
     public int get_moved() { return moved; }
+
+    private void move() { this.moved++; }
 
     public Car(String name) {
         this.name = name;
